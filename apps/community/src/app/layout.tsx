2e1086d4a10139c9ab12e9211de032cabae1b9ba
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-10-18 10:18:56 +0900

    feat: community, admin 프로젝트 설정 (#3)

---
+++ apps/community/src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import '../styles/globals.css';
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+});
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+});
+
+export const metadata: Metadata = {
+  title: '경기대학교 AI컴퓨터공학부',
+  description: '경기대학교 소프트웨어경영대학 AI컴퓨터공학부 공식 홈페이지',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        {children}
+      </body>
+    </html>
+  );
+}
