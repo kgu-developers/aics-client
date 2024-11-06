8fbabf85dde113085285a3d8cd770365fd8938b7
gwansikk <gwansik.kim@gwansik.dev>
2024-11-06 19:47:09 +0900

    feat: add spacing component

--- packages/design-system/src/components/index.ts
+++ packages/design-system/src/components/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from './button/button';
+export { default as Spacing } from './spacing/spacing';
 export { default as Spinner } from './spinner/spinner';
 export { default as Text } from './text/text';
 export { default as Badge } from './badge/badge';
