ff84586803c5b536d620a141ce3baea568eb4117
gwansikk <gwansik.kim@gwansik.dev>
2024-11-06 18:52:49 +0900

    feat: add badge component

--- packages/design-system/src/components/index.ts
+++ packages/design-system/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './button/button';
 export { default as Spinner } from './spinner/spinner';
 export { default as Text } from './text/text';
+export { default as Badge } from './badge/badge';
