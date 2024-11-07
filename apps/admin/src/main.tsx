0c63e64458eb5e899d05190e23657b21b92a355b
조성원 <tjddnjsjo@kakao.com>
2024-11-08 01:49:30 +0900

    chore(admin): add path alias

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import '../src/styles/globals.css';
+import App from '~/App.tsx';
+import '~/styles/globals.css';
 
 const rootElement = document.getElementById('root');
 
