3dd1cb2655ef49dee8b561b401cdc2348b5ee16f
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-11-13 18:14:33 +0900

    chore: 외부 url 허용

--- apps/community/next.config.ts
+++ apps/community/next.config.ts
@@ -2,11 +2,24 @@ import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
 import type { NextConfig } from 'next';
 
 const withVanillaExtract = createVanillaExtractPlugin({
-  identifiers: ({ hash }) => `_${hash}`
+  identifiers: ({ hash }) => `_${hash}`,
 });
 const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'kutis.kyonggi.ac.kr',
+        pathname: '/webkutis/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+      },
+    ],
   },
 };
 
