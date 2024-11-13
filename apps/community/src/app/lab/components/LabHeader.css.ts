7481515ba9dfcadfc943a6a929d3d9a2a421867e
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-11-13 17:08:50 +0900

    feat(community): add LabHeader component

---
+++ apps/community/src/app/lab/components/LabHeader.css.ts
@@ -0,0 +1,12 @@
+import { style } from '@vanilla-extract/css';
+
+const title = style({
+  fontSize: '2rem',
+  color: '#333D4B',
+});
+
+const description = style({
+  color: '#4E5968',
+});
+
+export { title, description };
