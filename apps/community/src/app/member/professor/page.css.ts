d524b69ba409e36dedc055178f2de2250754494c
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-11-14 17:27:13 +0900

    feat: page-header 추가

--- apps/community/src/app/member/professor/page.css.ts
+++ apps/community/src/app/member/professor/page.css.ts
@@ -4,6 +4,7 @@ export const professorListWrapper = style({
   display: 'grid',
   gap: 16,
   gridTemplateColumns: '1fr',
+  marginTop: 42,
 
   '@media': {
     'screen and (min-width: 640px)': {
--- apps/community/src/app/member/professor/page.tsx
+++ apps/community/src/app/member/professor/page.tsx
@@ -1,3 +1,4 @@
+import { PageHeader } from '~/components/page-header';
 import { ProfessorCard } from '~/components/professor-card';
 
 import * as styles from '~/app/member/professor/page.css';
@@ -6,13 +7,20 @@ import { PROFESSORS } from './mocks/professor';
 
 export default function Page(): JSX.Element {
   return (
-    <div className={styles.professorListWrapper}>
-      {PROFESSORS.map((professor) => (
-        <ProfessorCard
-          key={`professor-${professor.id}`}
-          professor={professor}
-        />
-      ))}
-    </div>
+    <section>
+      <PageHeader
+        title="교수진 소개"
+        description="경기대학교 AI컴퓨터공학부의 교수진을 소개해요"
+      />
+
+      <div className={styles.professorListWrapper}>
+        {PROFESSORS.map((professor) => (
+          <ProfessorCard
+            key={`professor-${professor.id}`}
+            professor={professor}
+          />
+        ))}
+      </div>
+    </section>
   );
 }
