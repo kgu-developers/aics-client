123ea17aa8f8dfdb4a911daa742316d1328b7984
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-11-14 19:21:16 +0900

    refactor: 페이지명 변경

--- apps/community/src/app/member/professor/page.tsx
+++ apps/community/src/app/member/professor/page.tsx
@@ -1,3 +1,5 @@
+import type { Metadata } from 'next';
+
 import { PageHeader } from '~/components/page-header';
 import { ProfessorCard } from '~/components/professor-card';
 
@@ -5,7 +7,12 @@ import * as styles from '~/app/member/professor/page.css';
 
 import { PROFESSORS } from './mocks/professor';
 
-export default function Page(): JSX.Element {
+export const metadata: Metadata = {
+  title: '교수진 소개 - 경기대학교 AI컴퓨터공학부',
+  description: '경기대학교 소프트웨어경영대학 AI컴퓨터공학부 공식 홈페이지',
+};
+
+export default function ProfessorPage() {
   return (
     <section>
       <PageHeader
