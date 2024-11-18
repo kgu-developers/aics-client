22111b7e6d8be223c102b699deb113300c408729
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-11-18 18:41:39 +0900

    feat(community): add AuthButton component

---
+++ apps/community/src/components/auth-button.css.ts
@@ -0,0 +1,20 @@
+import { themeVars } from '@aics-client/design-system/styles';
+import { style } from '@vanilla-extract/css';
+
+export const button = style({
+  padding: '0.8rem',
+  borderRadius: 8,
+  backgroundColor: themeVars.color.black,
+  color: themeVars.color.white,
+
+  ':hover': {
+    backgroundColor: themeVars.color.gray900,
+    transition: 'background-color 0.3s',
+  },
+
+  ':disabled': {
+    backgroundColor: themeVars.color.gray200,
+    color: themeVars.color.gray500,
+    cursor: 'not-allowed',
+  },
+});
