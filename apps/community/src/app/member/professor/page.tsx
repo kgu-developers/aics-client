e0a6db66c9326a39db635ae212440946eeaf3123
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-11-22 01:59:02 +0900

    feat(community): 교수진 페이지 모킹, design-token 토큰 적용 (#29)
    * feat: css 토큰 추가
    
    * refactor: 토큰 적용
    
    * feat: 교수진 소개 페이지 msw 적용
    
    * refactor: 타입 경로 수정
    
    * refactor: page.css에 토큰 적용

--- apps/community/src/app/member/professor/page.tsx
+++ apps/community/src/app/member/professor/page.tsx
@@ -1,18 +1,23 @@
 import type { Metadata } from 'next';
 
+import { ProfessorCard } from '~/components/member/professor/professor-card';
 import { PageHeader } from '~/components/page-header';
-import { ProfessorCard } from '~/components/professor-card';
+
+import { getProfessors } from './remote';
 
 import * as styles from '~/app/member/professor/page.css';
-
-import { PROFESSORS } from './mocks/professor';
 
 export const metadata: Metadata = {
   title: '교수진 소개 - 경기대학교 AI컴퓨터공학부',
   description: '경기대학교 소프트웨어경영대학 AI컴퓨터공학부 공식 홈페이지',
 };
 
-export default function ProfessorPage() {
+//** TODO: for mocking */
+export const dynamic = 'force-dynamic';
+
+export default async function ProfessorPage() {
+  const { data } = await getProfessors();
+
   return (
     <section>
       <PageHeader
@@ -21,7 +26,7 @@ export default function ProfessorPage() {
       />
 
       <div className={styles.professorListWrapper}>
-        {PROFESSORS.map((professor) => (
+        {data.map((professor) => (
           <ProfessorCard
             key={`professor-${professor.id}`}
             professor={professor}
