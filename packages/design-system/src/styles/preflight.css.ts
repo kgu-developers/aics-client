9e0e4afb705ee338d5d36d45f3fda8468c00596c
gwansikk <gwansik.kim@gwansik.dev>
2024-11-21 16:38:38 +0900

    fix(design-system): disable content property in preflight styles

--- packages/design-system/src/styles/preflight.css.ts
+++ packages/design-system/src/styles/preflight.css.ts
@@ -16,7 +16,7 @@ globalStyle('::before, ::after', {
   borderWidth: '0' /*2 */,
   borderStyle: 'solid' /*2 */,
   borderColor: 'currentColor' /*2 */,
-  content: '""',
+  // content: '""',
 });
 
 /*
