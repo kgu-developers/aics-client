79c652ad6be914fed5415529be539fb4f2c0d72f
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-11-22 03:19:52 +0900

    feat(community): update sign-in and sign-up forms with validation schema

--- apps/community/src/components/(auth)/signin/sign-in-form.tsx
+++ apps/community/src/components/(auth)/signin/sign-in-form.tsx
@@ -3,11 +3,26 @@
 import { Input } from '@aics-client/design-system';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
-import type { z } from 'zod';
+import { z } from 'zod';
 
 import { AuthButton } from '~/components/(auth)/auth-button';
 import * as styles from '~/components/(auth)/signin/sign-in-form.css';
-import { defaultValues, signInFormSchema } from '~/schemas/sign-in-form-schema';
+
+const signInFormSchema = z.object({
+  studentId: z.string().min(1, { message: '학번을 입력해주세요.' }),
+  password: z
+    .string()
+    .min(1, { message: '비밀번호를 입력해주세요.' })
+    .regex(
+      /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~!@#$%^&*])[a-zA-Z0-9~!@#$%^&*]{8,15}$/,
+      { message: '올바른 비밀번호 형식이 아닙니다.' },
+    ),
+});
+
+const defaultValues = {
+  studentId: '',
+  password: '',
+};
 
 function SignInForm() {
   const {
--- apps/community/src/components/(auth)/signup/sign-up-form.tsx
+++ apps/community/src/components/(auth)/signup/sign-up-form.tsx
@@ -3,11 +3,44 @@
 import { Input } from '@aics-client/design-system';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm, useWatch } from 'react-hook-form';
-import type { z } from 'zod';
+import { z } from 'zod';
 
 import { AuthButton } from '~/components/(auth)/auth-button';
 import * as styles from '~/components/(auth)/signup/sign-up-form.css';
-import { defaultValues, signUpFormSchema } from '~/schemas/sign-up-form-schema';
+
+const signUpFormSchema = z
+  .object({
+    studentId: z.string().min(1, { message: '학번을 입력해주세요.' }),
+    password: z
+      .string()
+      .min(1, { message: '비밀번호를 입력해주세요.' })
+      .regex(
+        /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~!@#$%^&*])[a-zA-Z0-9~!@#$%^&*]{8,15}$/,
+        { message: '올바른 비밀번호 형식이 아닙니다.' },
+      ),
+    confirm_password: z
+      .string()
+      .min(1, { message: '비밀번호 확인을 입력해주세요.' }),
+    name: z.string().min(1, { message: '이름을 입력해주세요.' }),
+    email: z
+      .string()
+      .min(1, { message: '이메일을 입력해주세요.' })
+      .email({ message: '올바른 이메일 형식이 아닙니다.' }),
+    phone: z.string().min(1, { message: '연락처를 입력해주세요.' }),
+  })
+  .refine((data) => data.password === data.confirm_password, {
+    message: '비밀번호가 일치하지 않습니다.',
+    path: ['confirm_password'],
+  });
+
+const defaultValues = {
+  studentId: '',
+  password: '',
+  confirm_password: '',
+  name: '',
+  email: '',
+  phone: '',
+};
 
 function SignUpForm() {
   const {
@@ -19,10 +52,6 @@ function SignUpForm() {
     resolver: zodResolver(signUpFormSchema),
     defaultValues,
   });
-
-  const onSubmit = (data: z.infer<typeof signUpFormSchema>) => {
-    console.log(data);
-  };
 
   const [studentId, password, confirmPassword, name, email, phone] = useWatch({
     control,
@@ -39,12 +68,15 @@ function SignUpForm() {
   const isFormValid =
     studentId && password && confirmPassword && name && email && phone;
 
+  const onSubmit = (data: z.infer<typeof signUpFormSchema>) => {
+    console.log(data);
+  };
+
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={styles.formWrapper}>
       <div className={styles.formField}>
         <Input
           {...register('studentId')}
-          id="studentId"
           type="text"
           label="학번"
           placeholder="학번을 입력해주세요"
@@ -55,7 +87,6 @@ function SignUpForm() {
       <div className={styles.formField}>
         <Input
           {...register('password')}
-          id="password"
           type="password"
           label="비밀번호"
           placeholder="영문자, 숫자, 특수문자 포함 8~15자"
@@ -63,7 +94,6 @@ function SignUpForm() {
         />
         <Input
           {...register('confirm_password')}
-          id="confirm_password"
           type="password"
           placeholder="비밀번호를 확인해 주세요"
           message={errors.confirm_password?.message}
@@ -73,7 +103,6 @@ function SignUpForm() {
       <div className={styles.formField}>
         <Input
           {...register('name')}
-          id="name"
           type="text"
           label="이름"
           placeholder="이름을 입력해주세요"
@@ -84,7 +113,6 @@ function SignUpForm() {
       <div className={styles.formField}>
         <Input
           {...register('email')}
-          id="email"
           type="text"
           label="이메일"
           placeholder="이메일을 입력해주세요"
@@ -95,7 +123,6 @@ function SignUpForm() {
       <div className={styles.formField}>
         <Input
           {...register('phone')}
-          id="phone"
           type="text"
           label="연락처"
           placeholder="연락처를 입력해주세요"
