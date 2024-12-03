68c591ec62d45c1428d43d60c74e98a46d1fe9c4
Gwansik Kim <gwansik.kim@gwansik.dev>
2024-12-03 19:26:46 +0900

    Update apps/community/src/app/my/page.tsx

--- apps/community/src/app/my/page.tsx
+++ apps/community/src/app/my/page.tsx
@@ -8,7 +8,6 @@ export const dynamic = 'force-dynamic';
 
 export default async function MyPage() {
   const { data } = await getMyProfile();
-  console.log(data);
 
   const userDetails = [
     { title: '이름', value: data.name },
