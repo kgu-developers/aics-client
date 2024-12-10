4d788a3c78e277f3156562ceab7a346225d1ba43
조성원 <tjddnjsjo@kakao.com>
2024-12-10 23:29:14 +0900

    feat(community): add news-carousel & notice-list into main page

--- apps/community/src/app/page.tsx
+++ apps/community/src/app/page.tsx
@@ -1,11 +1,18 @@
+import * as styles from '~/app/page.css';
 import { HeroCarousel } from '~/components/hero-carousel';
-import { PostCarousel } from '~/components/post-carousel';
+import { NewsCarousel } from '~/components/news-carousel';
+import { NoticeList } from '~/components/notice-list';
 
 export default function Home() {
   return (
-    <div>
-      <HeroCarousel />
-      <PostCarousel />
+    <div className={styles.wrapper}>
+      <section>
+        <HeroCarousel />
+      </section>
+      <section className={styles.section2}>
+        <NewsCarousel />
+        <NoticeList />
+      </section>
     </div>
   );
 }
