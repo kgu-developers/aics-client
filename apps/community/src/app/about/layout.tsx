853e7e31e99f4364dbfe037c238166e9001ba479
Minha Kang <118591632+m2na7@users.noreply.github.com>
2024-12-19 21:13:45 +0900

    refactor(community): update AsideNavigationMenu to accept paths as props and remove pages constant

--- apps/community/src/app/about/layout.tsx
+++ apps/community/src/app/about/layout.tsx
@@ -1,6 +1,12 @@
 import * as styles from '~/app/about/layout.css';
 import { AsideNavigationMenu } from '~/components/aside-navigation-menu';
 
+const ABOUT_PATHS = [
+  { title: '동아리 소개', url: '/about/club' },
+  { title: '찾아오시는 길', url: '/about/contact' },
+  { title: '학부 소개', url: '/about/dept' },
+];
+
 export default function AboutLayout({
   children,
 }: {
@@ -9,7 +15,7 @@ export default function AboutLayout({
   return (
     <div className={styles.layoutWrapper}>
       <div className={styles.navigationContainer}>
-        <AsideNavigationMenu />
+        <AsideNavigationMenu base="about" paths={ABOUT_PATHS} />
       </div>
       <div className={styles.content}>{children}</div>
     </div>
--- apps/community/src/components/aside-navigation-menu.tsx
+++ apps/community/src/components/aside-navigation-menu.tsx
@@ -1,28 +1,22 @@
 'use client';
 
 import Link from 'next/link';
-import { usePathname } from 'next/navigation';
 
 import * as styles from '~/components/aside-navigation-menu.css';
-import { PAGES } from '~/constants/pages';
 import { PATHMAP, type pathmapKey } from '~/constants/path';
 
-function AsideNavigationMenu() {
-  const pathname = usePathname();
-  const page = PAGES.find((page) => pathname.startsWith(page.base));
+interface Props {
+  paths: { title: string; url: string }[];
+  base: pathmapKey;
+}
 
+function AsideNavigationMenu({ paths, base }: Props) {
   return (
     <aside className={styles.navigationContainer}>
-      <h2 className={styles.navigationTitle}>
-        {PATHMAP[page?.id as pathmapKey]}
-      </h2>
+      <h2 className={styles.navigationTitle}>{PATHMAP[base]}</h2>
       <div className={styles.separator} />
-      {page?.path.map((path) => (
-        <Link
-          className={styles.navigationLink}
-          key={path.url}
-          href={page.base + path.url}
-        >
+      {paths.map((path) => (
+        <Link className={styles.navigationLink} key={path.url} href={path.url}>
           {path.title}
         </Link>
       ))}
