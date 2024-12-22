1106c8788798a66dd40e312ddaedf415bf3349b5
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2024-12-23 01:38:17 +0900

    refactor: react-query 적용 (mypage)

---
+++ apps/community/src/components/my/my-information.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+import { MY_PROFILE_QUERY_OPTIONS } from '~/apis/my/queries';
+
+import { MyInfoEditableProfileCard } from './my-info-editable-profile-card';
+import { MyInfoProfileCard } from './my-info-profile-card';
+
+function MyInformation() {
+  const { data } = useSuspenseQuery(MY_PROFILE_QUERY_OPTIONS.ALL());
+  const userDetails = [
+    { title: '이름', value: data.data.name },
+    { title: '학번', value: data.data.id },
+    { title: '구분', value: data.data.role },
+    { title: '전공', value: data.data.major },
+  ];
+
+  const userEditableDetails = [
+    { title: '전화번호', value: data.data.phone },
+    { title: '이메일', value: data.data.email },
+  ];
+  return (
+    <>
+      <MyInfoProfileCard data={userDetails} />
+      <MyInfoEditableProfileCard data={userEditableDetails} />
+    </>
+  );
+}
+
+export { MyInformation };
