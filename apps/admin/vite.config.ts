b5e62c4e0f7a6565a5bc3146910a93352e81084d
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2025-02-13 17:12:05 +0900

    chore: import alias 수정

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,11 +1,12 @@
+import path from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: [{ find: '~', replacement: './src' }],
+    alias: { '~': path.resolve(__dirname, 'src') },
   },
 });
