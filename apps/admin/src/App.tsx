1619603cdc4470c6140d722e03bf5583189cf615
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2025-02-13 20:26:46 +0900

    chore: init react-query & react-query-devtools

--- apps/admin/src/App.tsx
+++ apps/admin/src/App.tsx
@@ -1,11 +1,18 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router';
+
 import Router from './router/router';
+import { queryClient } from './utils/get-query-client';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
