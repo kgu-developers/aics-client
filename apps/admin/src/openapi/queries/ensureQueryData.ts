c7fb7c409a1a35fa4841885dedb150d407ef76dc
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2025-02-16 22:51:40 +0900

    chore: init openapi react-query codegen

---
+++ apps/admin/src/openapi/queries/ensureQueryData.ts
@@ -0,0 +1,19 @@
+// generated with @7nohe/openapi-react-query-codegen@1.6.2
+
+import type { QueryClient } from '@tanstack/react-query';
+import { UserService } from '../requests/services.gen';
+import * as Common from './common';
+export const ensureUseUserServiceGetApiV1UsersData = (
+  queryClient: QueryClient,
+  {
+    page,
+    size,
+  }: {
+    page: number;
+    size: number;
+  },
+) =>
+  queryClient.ensureQueryData({
+    queryKey: Common.UseUserServiceGetApiV1UsersKeyFn({ page, size }),
+    queryFn: () => UserService.getApiV1Users({ page, size }),
+  });
---
+++ apps/admin/src/openapi/queries/queries.ts
@@ -0,0 +1,713 @@
+// generated with @7nohe/openapi-react-query-codegen@1.6.2
+
+import {
+  type UseMutationOptions,
+  type UseQueryOptions,
+  useMutation,
+  useQuery,
+} from '@tanstack/react-query';
+import {
+  AboutService,
+  CarouselService,
+  ClubService,
+  FileService,
+  LabService,
+  PostService,
+  ProfessorService,
+  UserService,
+} from '../requests/services.gen';
+import type {
+  AboutCreateRequest,
+  AboutUpdateRequest,
+  CarouselRequest,
+  ClubRequest,
+  LabRequest,
+  PostRequest,
+  ProfessorRequest,
+} from '../requests/types.gen';
+import * as Common from './common';
+export const useUserServiceGetApiV1Users = <
+  TData = Common.UserServiceGetApiV1UsersDefaultResponse,
+  TError = unknown,
+  TQueryKey extends Array<unknown> = unknown[],
+>(
+  {
+    page,
+    size,
+  }: {
+    page: number;
+    size: number;
+  },
+  queryKey?: TQueryKey,
+  options?: Omit<UseQueryOptions<TData, TError>, 'queryKey' | 'queryFn'>,
+) =>
+  useQuery<TData, TError>({
+    queryKey: Common.UseUserServiceGetApiV1UsersKeyFn({ page, size }, queryKey),
+    queryFn: () => UserService.getApiV1Users({ page, size }) as TData,
+    ...options,
+  });
+export const useProfessorServicePostApiV1Professors = <
+  TData = Common.ProfessorServicePostApiV1ProfessorsMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        requestBody: ProfessorRequest;
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      requestBody: ProfessorRequest;
+    },
+    TContext
+  >({
+    mutationFn: ({ requestBody }) =>
+      ProfessorService.postApiV1Professors({
+        requestBody,
+      }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const usePostServicePostApiV1Posts = <
+  TData = Common.PostServicePostApiV1PostsMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        fileId?: number;
+        requestBody: PostRequest;
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      fileId?: number;
+      requestBody: PostRequest;
+    },
+    TContext
+  >({
+    mutationFn: ({ fileId, requestBody }) =>
+      PostService.postApiV1Posts({
+        fileId,
+        requestBody,
+      }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const useLabServicePostApiV1Labs = <
+  TData = Common.LabServicePostApiV1LabsMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        fileId?: number;
+        requestBody: LabRequest;
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      fileId?: number;
+      requestBody: LabRequest;
+    },
+    TContext
+  >({
+    mutationFn: ({ fileId, requestBody }) =>
+      LabService.postApiV1Labs({
+        fileId,
+        requestBody,
+      }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const useFileServicePostApiV1FilesPost = <
+  TData = Common.FileServicePostApiV1FilesPostMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        formData?: { file: Blob | File };
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      formData?: { file: Blob | File };
+    },
+    TContext
+  >({
+    mutationFn: ({ formData }) =>
+      FileService.postApiV1FilesPost({ formData }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const useFileServicePostApiV1FilesLab = <
+  TData = Common.FileServicePostApiV1FilesLabMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        formData?: { file: Blob | File };
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      formData?: { file: Blob | File };
+    },
+    TContext
+  >({
+    mutationFn: ({ formData }) =>
+      FileService.postApiV1FilesLab({ formData }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const useFileServicePostApiV1FilesCarousel = <
+  TData = Common.FileServicePostApiV1FilesCarouselMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        formData?: { file: Blob | File };
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      formData?: { file: Blob | File };
+    },
+    TContext
+  >({
+    mutationFn: ({ formData }) =>
+      FileService.postApiV1FilesCarousel({
+        formData,
+      }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const useFileServicePostApiV1FilesAbout = <
+  TData = Common.FileServicePostApiV1FilesAboutMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        formData?: { file: Blob | File };
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      formData?: { file: Blob | File };
+    },
+    TContext
+  >({
+    mutationFn: ({ formData }) =>
+      FileService.postApiV1FilesAbout({
+        formData,
+      }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const useClubServicePostApiV1Clubs = <
+  TData = Common.ClubServicePostApiV1ClubsMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        requestBody: ClubRequest;
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      requestBody: ClubRequest;
+    },
+    TContext
+  >({
+    mutationFn: ({ requestBody }) =>
+      ClubService.postApiV1Clubs({ requestBody }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const useCarouselServicePostApiV1Carousels = <
+  TData = Common.CarouselServicePostApiV1CarouselsMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        fileId: number;
+        requestBody: CarouselRequest;
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      fileId: number;
+      requestBody: CarouselRequest;
+    },
+    TContext
+  >({
+    mutationFn: ({ fileId, requestBody }) =>
+      CarouselService.postApiV1Carousels({
+        fileId,
+        requestBody,
+      }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const useAboutServicePostApiV1Abouts = <
+  TData = Common.AboutServicePostApiV1AboutsMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        requestBody: AboutCreateRequest;
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      requestBody: AboutCreateRequest;
+    },
+    TContext
+  >({
+    mutationFn: ({ requestBody }) =>
+      AboutService.postApiV1Abouts({
+        requestBody,
+      }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const useProfessorServicePatchApiV1ProfessorsById = <
+  TData = Common.ProfessorServicePatchApiV1ProfessorsByIdMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        id: number;
+        requestBody: ProfessorRequest;
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      id: number;
+      requestBody: ProfessorRequest;
+    },
+    TContext
+  >({
+    mutationFn: ({ id, requestBody }) =>
+      ProfessorService.patchApiV1ProfessorsById({
+        id,
+        requestBody,
+      }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const usePostServicePatchApiV1PostsByPostId = <
+  TData = Common.PostServicePatchApiV1PostsByPostIdMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        postId: number;
+        requestBody: PostRequest;
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      postId: number;
+      requestBody: PostRequest;
+    },
+    TContext
+  >({
+    mutationFn: ({ postId, requestBody }) =>
+      PostService.patchApiV1PostsByPostId({
+        postId,
+        requestBody,
+      }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const usePostServicePatchApiV1PostsByPostIdPin = <
+  TData = Common.PostServicePatchApiV1PostsByPostIdPinMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        postId: number;
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      postId: number;
+    },
+    TContext
+  >({
+    mutationFn: ({ postId }) =>
+      PostService.patchApiV1PostsByPostIdPin({
+        postId,
+      }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const usePostServicePatchApiV1PostsByPostIdDelete = <
+  TData = Common.PostServicePatchApiV1PostsByPostIdDeleteMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        postId: number;
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      postId: number;
+    },
+    TContext
+  >({
+    mutationFn: ({ postId }) =>
+      PostService.patchApiV1PostsByPostIdDelete({
+        postId,
+      }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const useLabServicePatchApiV1LabsById = <
+  TData = Common.LabServicePatchApiV1LabsByIdMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        id: number;
+        requestBody: LabRequest;
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      id: number;
+      requestBody: LabRequest;
+    },
+    TContext
+  >({
+    mutationFn: ({ id, requestBody }) =>
+      LabService.patchApiV1LabsById({
+        id,
+        requestBody,
+      }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const useClubServicePatchApiV1ClubsById = <
+  TData = Common.ClubServicePatchApiV1ClubsByIdMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        id: number;
+        requestBody: ClubRequest;
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      id: number;
+      requestBody: ClubRequest;
+    },
+    TContext
+  >({
+    mutationFn: ({ id, requestBody }) =>
+      ClubService.patchApiV1ClubsById({
+        id,
+        requestBody,
+      }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const useAboutServicePatchApiV1AboutsById = <
+  TData = Common.AboutServicePatchApiV1AboutsByIdMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        id: number;
+        requestBody: AboutUpdateRequest;
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      id: number;
+      requestBody: AboutUpdateRequest;
+    },
+    TContext
+  >({
+    mutationFn: ({ id, requestBody }) =>
+      AboutService.patchApiV1AboutsById({
+        id,
+        requestBody,
+      }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const useProfessorServiceDeleteApiV1ProfessorsById = <
+  TData = Common.ProfessorServiceDeleteApiV1ProfessorsByIdMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        id: number;
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      id: number;
+    },
+    TContext
+  >({
+    mutationFn: ({ id }) =>
+      ProfessorService.deleteApiV1ProfessorsById({
+        id,
+      }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const useLabServiceDeleteApiV1LabsById = <
+  TData = Common.LabServiceDeleteApiV1LabsByIdMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        id: number;
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      id: number;
+    },
+    TContext
+  >({
+    mutationFn: ({ id }) =>
+      LabService.deleteApiV1LabsById({ id }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const useClubServiceDeleteApiV1ClubsById = <
+  TData = Common.ClubServiceDeleteApiV1ClubsByIdMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        id: number;
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      id: number;
+    },
+    TContext
+  >({
+    mutationFn: ({ id }) =>
+      ClubService.deleteApiV1ClubsById({ id }) as unknown as Promise<TData>,
+    ...options,
+  });
+export const useCarouselServiceDeleteApiV1CarouselsById = <
+  TData = Common.CarouselServiceDeleteApiV1CarouselsByIdMutationResult,
+  TError = unknown,
+  TContext = unknown,
+>(
+  options?: Omit<
+    UseMutationOptions<
+      TData,
+      TError,
+      {
+        id: number;
+      },
+      TContext
+    >,
+    'mutationFn'
+  >,
+) =>
+  useMutation<
+    TData,
+    TError,
+    {
+      id: number;
+    },
+    TContext
+  >({
+    mutationFn: ({ id }) =>
+      CarouselService.deleteApiV1CarouselsById({
+        id,
+      }) as unknown as Promise<TData>,
+    ...options,
+  });
