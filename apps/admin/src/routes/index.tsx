dbe56044d698806d8c2e102c7a101d7bea757e46
NoaH <85290394+noahyunjun@users.noreply.github.com>
2025-02-20 16:43:53 +0900

    feat(admin): publish admin login page (#77)
    * feat(admin): sign-in page add
    
    * refactor: index Page delete
    
    * fix: tailwind 미적용 버그 수정
    
    * feat(admin): antd dependency add
    
    * fix(admin): ant warning in react19
    - ant가 react 19에서 아직 최적화되지 않은 문제점 발견 및 해결
    
    * feat(admin): 공백 제거 로직 추가
    
    * refactor(admin): index페이지로 전환
    - 리디렉션 대신 index 페이지로 전환
    
    * chore(admin): routeTree파일 추적 삭제
    - 서버 재실행시 자동 생성 파일
    
    - 라우팅 페이지가 많아질수록 파일의 크기가 커질것으로 보임
    
    * chore: upgrade pnpm
    
    * style: format code
    
    ---------
    
    Co-authored-by: Seongwon Jo <tjddnjsjo@kakao.com>

--- apps/admin/src/routes/index.tsx
+++ apps/admin/src/routes/index.tsx
@@ -1,9 +1,74 @@
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { createFileRoute } from '@tanstack/react-router';
+import { Button, Card, Form, Input, Typography } from 'antd';
 
 export const Route = createFileRoute('/')({
   component: SignInPage,
 });
 
+interface FormValues {
+  ID: string;
+  password: string;
+}
+
 function SignInPage() {
-  return <div>signin</div>;
+  const onFinish = (values: FormValues) => {
+    console.log('로그인 정보:', values);
+  };
+
+  const [form] = Form.useForm();
+
+  const removeSpace = (fieldName: 'id' | 'password') => {
+    return (e: React.ChangeEvent<HTMLInputElement>) => {
+      const newValue = e.target.value.replace(/\s/g, '');
+      form.setFieldsValue({ [fieldName]: newValue });
+    };
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <Card className="w-96 shadow-lg">
+        <Typography.Title level={3} className="text-center">
+          로그인
+        </Typography.Title>
+        <Form
+          form={form}
+          name="login"
+          onFinish={onFinish}
+          layout="vertical"
+          initialValues={{ id: '', password: '' }}
+        >
+          <Form.Item
+            label="아이디"
+            name="id"
+            rules={[{ required: true, message: '아이디를 입력하세요!' }]}
+          >
+            <Input
+              prefix={<UserOutlined />}
+              placeholder="아이디 입력"
+              onChange={removeSpace('id')}
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="비밀번호"
+            name="password"
+            rules={[{ required: true, message: '비밀번호를 입력하세요!' }]}
+          >
+            <Input.Password
+              prefix={<LockOutlined />}
+              placeholder="비밀번호 입력"
+              onChange={removeSpace('password')}
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="w-full">
+              로그인
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 }
