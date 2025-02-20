eadca5fa0d025c3076625292f70e1a5a5e25da5d
Seongwon Jo <tjddnjsjo@kakao.com>
2025-02-20 16:32:06 +0900

    style: format code

--- packages/design-system/.storybook/main.ts
+++ packages/design-system/.storybook/main.ts
@@ -1,33 +1,33 @@
-import { dirname, join } from "node:path";
-import type { StorybookConfig } from "@storybook/react-vite";
-import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import { dirname, join } from 'node:path';
+import type { StorybookConfig } from '@storybook/react-vite';
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 function getAbsolutePath(value: string) {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, 'package.json')));
 }
 const config: StorybookConfig = {
   stories: [
-    "../src/stories**/*.mdx",
-    "../src/components/**/*.stories.@(ts|tsx)",
+    '../src/stories**/*.mdx',
+    '../src/components/**/*.stories.@(ts|tsx)',
   ],
   addons: [
-    getAbsolutePath("@storybook/addon-links"),
-    getAbsolutePath("@storybook/addon-essentials"),
-    getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-interactions'),
   ],
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: getAbsolutePath('@storybook/react-vite'),
     options: {},
   },
   core: {
-    builder: getAbsolutePath("@storybook/builder-vite"),
+    builder: getAbsolutePath('@storybook/builder-vite'),
   },
   async viteFinal(config) {
     config.plugins = config.plugins || [];
     config.plugins.push(
       vanillaExtractPlugin({
         identifiers: ({ hash }) => `_${hash}`,
-      })
+      }),
     );
     return config;
   },
