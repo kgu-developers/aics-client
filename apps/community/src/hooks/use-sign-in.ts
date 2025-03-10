b7ce2a1826ada41c73517bdddfcb4dd6a0fb6965
HamHyeonJun <hyunjun003@naver.com>
2025-03-10 20:09:15 +0900

    refactor(community): 불필요 비동기처리 삭제

--- apps/community/src/hooks/use-sign-in.ts
+++ apps/community/src/hooks/use-sign-in.ts
@@ -19,8 +19,8 @@ const useSignIn = () => {
   const router = useRouter();
 
   return useMutation({
-    mutationFn: async (data: z.infer<typeof signInFormSchema>) => {
-      const res = await http.post<typeof data, Tokens>(END_POINT.SIGN_IN, data);
+    mutationFn: (data: z.infer<typeof signInFormSchema>) => {
+      const res = http.post<typeof data, Tokens>(END_POINT.SIGN_IN, data);
       return res;
     },
     onError: (e) => {
