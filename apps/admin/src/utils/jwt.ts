2288fea0b7b4b6cc3c9b0e8ea04a981aae0efc0a
HamHyeonJun <hyunjun003@naver.com>
2025-03-10 20:54:39 +0900

    refactor(admin): jwt토큰 디코딩 예외처리

--- apps/admin/src/utils/jwt.ts
+++ apps/admin/src/utils/jwt.ts
@@ -1,6 +1,6 @@
 export const decodeJwt = (token: string) => {
   try {
-    const base64Url = token.split('.')[1];
+    const base64Url = token.split('.')[1] ?? '';
     const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     const jsonPayload = decodeURIComponent(
       atob(base64)
