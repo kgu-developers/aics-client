e90b6f3eb40c9e08a74763f82b6ca0c58580533e
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2025-03-11 23:49:28 +0900

    refactor: use-auth에서 refreshTokens 분리

---
+++ apps/admin/src/hooks/use-refresh-token.ts
@@ -0,0 +1,40 @@
+import { useMutation } from '@tanstack/react-query';
+import { END_POINT } from '~/constants/api';
+import { getToken } from '~/utils/api';
+import { authServices } from '~/utils/auth';
+import { authHttp } from '~/utils/http';
+import type { Tokens } from './use-sign-in';
+
+interface RefreshToken {
+  refreshToken: string;
+}
+
+const useRefreshTokens = () => {
+  const { setTokens, logout } = authServices();
+
+  const refreshMutation = useMutation({
+    mutationFn: () => {
+      const [accessToken, refreshToken] = getToken();
+
+      if (!accessToken || !refreshToken) {
+        throw new Error('토큰이 존재하지 않습니다');
+      }
+
+      const response = authHttp.post<RefreshToken>(END_POINT.REISSUE, {
+        json: { refreshToken },
+      });
+      return response.json<Tokens>();
+    },
+    onSuccess: (newTokens) => {
+      setTokens(newTokens);
+    },
+    onError: () => {
+      alert('오류가 발생하여 로그아웃합니다.');
+      logout();
+    },
+  });
+
+  return refreshMutation;
+};
+
+export { type RefreshToken, useRefreshTokens };
