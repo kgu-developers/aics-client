afb6700a91364b22610f3d2404a669ce3b976aef
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2025-03-20 17:27:09 +0900

    feat: 게시물 수정 페이지 추가

---
+++ apps/admin/src/routes/news/edit/$postId.tsx
@@ -0,0 +1,14 @@
+import { createFileRoute, useMatch } from '@tanstack/react-router';
+import { useGetPostById } from '~/apis/community/queries/suspense';
+import { EditPostField } from '~/components/posts/edit-post-field';
+
+export const Route = createFileRoute('/news/edit/$postId')({
+  component: PostEditPage,
+});
+
+function PostEditPage() {
+  const { params } = useMatch({ from: '/news/edit/$postId' });
+  const { data: post } = useGetPostById({ postId: Number(params.postId) });
+
+  return <EditPostField post={post} editMode />;
+}
