24bfbd076204d702965df7aebd412d5242d96cb6
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2025-04-01 13:01:19 +0900

    refactor(admin): search-bar 컴포넌트 antd로 대체

--- apps/admin/src/components/posts/search-bar.tsx
+++ apps/admin/src/components/posts/search-bar.tsx
@@ -1,14 +1,13 @@
 import { useNavigate } from '@tanstack/react-router';
-import { SearchIcon } from 'lucide-react';
-import { useState } from 'react';
+import { Input } from 'antd';
 
 interface SearchBarProps {
   defaultValue: string;
 }
 
 function SearchBar({ defaultValue }: SearchBarProps) {
-  const [searchText, setSearchText] = useState('');
   const navigate = useNavigate({ from: '/notice' });
+  const { Search } = Input;
 
   const handleSearch = (keyword: string) => {
     navigate({
@@ -16,29 +15,15 @@ function SearchBar({ defaultValue }: SearchBarProps) {
     });
   };
 
-  const handlePressEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      handleSearch(searchText);
-    }
-  };
-
   return (
-    <div className="self-center min-w-[40vw] gap-4 p-2 flex justify-between items-center border border-gray-300 rounded-lg">
-      <input
-        type="text"
+    <div className="self-center min-w-[40vw]">
+      <Search
+        placeholder="사용자명을 입력하세요"
         defaultValue={defaultValue}
-        onChange={(e) => setSearchText(e.target.value)}
-        onKeyUp={handlePressEnter}
-        placeholder="검색어를 입력하세요"
-        className="w-full p-1 outline-none"
+        allowClear
+        size="large"
+        onSearch={handleSearch}
       />
-      <button
-        type="button"
-        onClick={() => handleSearch(searchText)}
-        className="p-1 transition-colors duration-150 rounded-lg cursor-pointer hover:bg-gray-200"
-      >
-        <SearchIcon />
-      </button>
     </div>
   );
 }
