674524605e88f1f2656fd2dccb759a472ef28558
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2025-04-01 13:13:50 +0900

    feat(admin): 회원 관리 페이지 추가 (#119)
    * feat(admin): 회원 관리 페이지 추가
    
    * refactor(admin): suspense 범위 수정
    
    * chore(admin): remove unused import
    
    * refactor(admin): search-bar 컴포넌트 antd로 대체

---
+++ apps/admin/src/routes/user/index.tsx
@@ -0,0 +1,43 @@
+import { createFileRoute, useSearch } from '@tanstack/react-router';
+import { Spin } from 'antd';
+import { Suspense } from 'react';
+import { useUserServiceGetApiV1Users } from '~/apis/admin/queries';
+import { SearchBar } from '~/components/user/search-bar';
+import { UserList } from '~/components/user/user-list';
+
+export const Route = createFileRoute('/user/')({
+  component: UserPage,
+  validateSearch: (search: { page?: string; query?: string }) => ({
+    page: Number(search.page) || 0,
+    query: typeof search.query === 'string' ? search.query : '',
+  }),
+});
+
+const LIST_SIZE = 10;
+
+function UserPage() {
+  const { page: currentPage, query: name } = useSearch({
+    from: '/user/',
+  });
+
+  const { data: userList } = useUserServiceGetApiV1Users({
+    page: currentPage,
+    size: LIST_SIZE,
+    name: name,
+  });
+
+  return (
+    <section className="flex flex-col gap-3 px-16">
+      <SearchBar defaultValue={name} />
+      <Suspense fallback={<Spin />}>
+        {userList?.contents && (
+          <UserList
+            data={userList.contents}
+            currentPage={currentPage}
+            total={userList.pageable.totalElements}
+          />
+        )}
+      </Suspense>
+    </section>
+  );
+}
