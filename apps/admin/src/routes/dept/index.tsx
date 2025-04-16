1ab9c91fa6939560172fc2c9d13430063e1abc66
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2025-04-16 17:08:05 +0900

    refactor(admin): 수정 버튼 위치 수정

--- apps/admin/src/routes/dept/index.tsx
+++ apps/admin/src/routes/dept/index.tsx
@@ -20,7 +20,16 @@ function DeptPage() {
 
   return (
     <section className="flex flex-col w-full gap-8">
-      <h1 className="text-3xl font-bold ">학부 소개</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold ">학부 소개</h1>
+        <Link to={PATH.EDIT_DEPT} className="self-end">
+          {isDeptIntroEmpty ? (
+            <Button type="primary">작성하기</Button>
+          ) : (
+            <Button type="primary">수정하기</Button>
+          )}
+        </Link>
+      </div>
       {isDeptIntroEmpty ? (
         <span className="border-y py-8 border-gray-200">
           작성된 학부 소개가 없습니다.
@@ -32,14 +41,6 @@ function DeptPage() {
           dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data.content) }}
         />
       )}
-
-      <Link to={PATH.EDIT_DEPT} className="self-end">
-        {isDeptIntroEmpty ? (
-          <Button type="primary">작성하기</Button>
-        ) : (
-          <Button type="primary">수정하기</Button>
-        )}
-      </Link>
     </section>
   );
 }
--- apps/admin/src/routes/directions/index.tsx
+++ apps/admin/src/routes/directions/index.tsx
@@ -20,7 +20,16 @@ function DirectionsPage() {
 
   return (
     <section className="flex flex-col w-full gap-8">
-      <h1 className="text-3xl font-bold ">찾아오시는 길</h1>
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold ">찾아오시는 길</h1>
+        <Link to={PATH.EDIT_DEPT} className="self-end">
+          {isDirectionsEmpty ? (
+            <Button type="primary">작성하기</Button>
+          ) : (
+            <Button type="primary">수정하기</Button>
+          )}
+        </Link>
+      </div>
       {isDirectionsEmpty ? (
         <span className="border-y py-8 border-gray-200">
           작성된 안내가 없습니다.
