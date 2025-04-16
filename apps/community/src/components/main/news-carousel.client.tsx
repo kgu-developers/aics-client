b1a50491360ecc7178998d388051db9a1a37e3cd
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2025-04-16 19:00:37 +0900

    chore(community): remove unused annotation

--- apps/community/src/components/main/news-carousel.client.tsx
+++ apps/community/src/components/main/news-carousel.client.tsx
@@ -2,7 +2,6 @@
 
 import dynamic from 'next/dynamic';
 
-// 클라이언트 전용 dynamic import
 const NewsCarousel = dynamic(() => import('./news-carousel'), {
   ssr: false,
 });
--- apps/community/src/components/main/notice-list.client.tsx
+++ apps/community/src/components/main/notice-list.client.tsx
@@ -2,7 +2,6 @@
 
 import dynamic from 'next/dynamic';
 
-// 클라이언트 전용 dynamic import
 const NoticeList = dynamic(() => import('./notice-list'), {
   ssr: false,
 });
