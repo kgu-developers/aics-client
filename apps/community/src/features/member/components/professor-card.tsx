9eb4eab943274fb97071cc5285cd44a5ba62332a
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2025-04-24 10:18:43 +0900

    refactor(community): improve professor-card structure

--- apps/community/src/features/member/components/professor-card.tsx
+++ apps/community/src/features/member/components/professor-card.tsx
@@ -2,26 +2,53 @@ import Image from 'next/image'
 
 import * as styles from '~/features/member/components/professor-card.css'
 import type { Professor } from '~/features/member/services/remote'
+import AltImage from '~/shared/assets/images/alt.png'
 
 function ProfessorCard({ professor }: { professor: Professor }) {
   return (
-    <div className={styles.card}>
-      <Image
-        src={professor.img ?? 'https://placehold.co/128'}
-        width={100}
-        height={100}
-        className={styles.avatarImage}
-        alt={professor.name}
+    <section className={styles.card}>
+      <AvatarImage src={professor.img} name={professor.name} />
+      <CardContent name={professor.name} type={professor.type} />
+      <CardFooter
+        contact={professor.contact}
+        email={professor.email}
+        officeLoc={professor.officeLoc}
       />
-      <div className={styles.cardContent}>
-        <h2 className={styles.professorName}>{professor.name}</h2>
-        <p className={styles.professorType}>{professor.type}</p>
-      </div>
-      <div className={styles.cardFooter}>
-        <p className={styles.professorContact}>{professor.contact}</p>
-        <p className={styles.professorEmail}>{professor.email}</p>
-        <p>{professor.officeLoc}</p>
-      </div>
+    </section>
+  )
+}
+
+function AvatarImage({ src, name }: { src: string | undefined; name: string }) {
+  return (
+    <Image
+      src={src ?? AltImage}
+      width={100}
+      height={100}
+      className={styles.avatarImage}
+      alt={name}
+    />
+  )
+}
+
+function CardContent({ name, type }: { name: string; type: string }) {
+  return (
+    <div className={styles.cardContent}>
+      <h2 className={styles.professorName}>{name}</h2>
+      <p className={styles.professorType}>{type}</p>
+    </div>
+  )
+}
+
+function CardFooter({
+  contact,
+  email,
+  officeLoc,
+}: { contact: string; email: string; officeLoc: string }) {
+  return (
+    <div className={styles.cardFooter}>
+      <p className={styles.professorContact}>{contact}</p>
+      <p className={styles.professorEmail}>{email}</p>
+      <p>{officeLoc}</p>
     </div>
   )
 }
