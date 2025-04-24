9b92499838849eda35bda7bff30c3111dcce0a75
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2025-04-24 17:52:39 +0900

    feat(community): add contact-info-section

---
+++ apps/community/src/features/contact/components/contact-info-section.tsx
@@ -0,0 +1,21 @@
+'use client'
+
+import { useSuspenseQuery } from '@tanstack/react-query'
+import DOMPurify from 'dompurify'
+
+import { CONTACT_QUERY_OPTIONS } from '~/features/contact/services/queries'
+
+function ContactInfoSection() {
+  const { data } = useSuspenseQuery(CONTACT_QUERY_OPTIONS.ALL())
+
+  return (
+    <section>
+      <div
+        // biome-ignore lint/security/noDangerouslySetInnerHtml: DOMPurify 적용
+        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data.content) }}
+      />
+    </section>
+  )
+}
+
+export { ContactInfoSection }
