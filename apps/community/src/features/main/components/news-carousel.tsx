2090eaca452fa18f155f898d6d94f123487d1fe3
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2025-04-24 10:34:34 +0900

    refactor(community): improve news-carousel structure

--- apps/community/src/features/main/components/news-carousel.tsx
+++ apps/community/src/features/main/components/news-carousel.tsx
@@ -10,6 +10,7 @@ import useEmblaCarousel from 'embla-carousel-react'
 
 import * as styles from '~/features/main/components/news-carousel.css'
 import { MAIN_QUERY_OPTIONS } from '~/features/main/services/queries'
+import type { Post } from '~/features/main/services/remote'
 import AltImage from '~/shared/assets/images/alt.png'
 import { CarouselDots } from '~/shared/components/carousel/carousel-dots'
 import { PATH } from '~/shared/constants/path'
@@ -41,29 +42,35 @@ function NewsCarousel() {
       <div className={styles.viewport} ref={emblaRef}>
         <div className={styles.slides}>
           {recentNews.map((post) => (
-            <Link
-              key={`news-${post.postId}`}
-              href={PATH.NEWS_DETAIL(post.postId)}
-              className={styles.link}
-            >
-              <div className={styles.slide}>
-                <div className={styles.image}>
-                  <Image src={AltImage} alt="preview-image" fill />
-                </div>
-                <h3 className={styles.slideTitle}>{post.title}</h3>
-                <div
-                  className={styles.slideDescription}
-                  // biome-ignore lint/security/noDangerouslySetInnerHtml: DOMPurify 적용
-                  dangerouslySetInnerHTML={{
-                    __html: DOMPurify.sanitize(post.description),
-                  }}
-                />
-              </div>
-            </Link>
+            <NewsCard key={`news-${post.postId}`} post={post} />
           ))}
         </div>
       </div>
     </section>
+  )
+}
+
+function NewsCard({ post }: { post: Post }) {
+  return (
+    <Link
+      key={`news-${post.postId}`}
+      href={PATH.NEWS_DETAIL(post.postId)}
+      className={styles.link}
+    >
+      <div className={styles.slide}>
+        <div className={styles.image}>
+          <Image src={AltImage} alt="preview-image" fill />
+        </div>
+        <h3 className={styles.slideTitle}>{post.title}</h3>
+        <div
+          className={styles.slideDescription}
+          // biome-ignore lint/security/noDangerouslySetInnerHtml: DOMPurify 적용
+          dangerouslySetInnerHTML={{
+            __html: DOMPurify.sanitize(post.description),
+          }}
+        />
+      </div>
+    </Link>
   )
 }
 
