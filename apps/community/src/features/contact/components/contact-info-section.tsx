ed0856692fa1ccc9bf2b90c13eba605a8c3502fb
JaeguJaegu <118053865+SunwooJaeho@users.noreply.github.com>
2025-06-11 23:58:16 +0900

    feat(community): global-style을 통한 tiptap 에디터 텍스트 스타일링 적용

--- apps/community/src/features/contact/components/contact-info-section.tsx
+++ apps/community/src/features/contact/components/contact-info-section.tsx
@@ -3,18 +3,18 @@
 import { useSuspenseQuery } from '@tanstack/react-query'
 import DOMPurify from 'isomorphic-dompurify'
 
+import * as styles from '~/features/contact/components/content-info-section.css'
 import { CONTACT_QUERY_OPTIONS } from '~/features/contact/services/queries'
 
 function ContactInfoSection() {
   const { data } = useSuspenseQuery(CONTACT_QUERY_OPTIONS.ALL())
 
   return (
-    <section>
-      <div
-        // biome-ignore lint/security/noDangerouslySetInnerHtml: DOMPurify 적용
-        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data.content) }}
-      />
-    </section>
+    <section
+      className={styles.information}
+      // biome-ignore lint/security/noDangerouslySetInnerHtml: DOMPurify 적용
+      dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data.content) }}
+    />
   )
 }
 
--- apps/community/src/features/dept/components/dept-info-section.tsx
+++ apps/community/src/features/dept/components/dept-info-section.tsx
@@ -3,18 +3,18 @@
 import { useSuspenseQuery } from '@tanstack/react-query'
 import DOMPurify from 'isomorphic-dompurify'
 
+import * as styles from '~/features/dept/components/dept-info-section.css'
 import { DEPT_QUERY_OPTIONS } from '~/features/dept/services/queries'
 
 function DeptInfoSection() {
   const { data } = useSuspenseQuery(DEPT_QUERY_OPTIONS.ALL())
 
   return (
-    <section>
-      <div
-        // biome-ignore lint/security/noDangerouslySetInnerHtml: DOMPurify 적용
-        dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data.content) }}
-      />
-    </section>
+    <section
+      className={styles.information}
+      // biome-ignore lint/security/noDangerouslySetInnerHtml: DOMPurify 적용
+      dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(data.content) }}
+    />
   )
 }
 
