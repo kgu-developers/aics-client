09d52b3eeddc9de27dcdd9edd1e31500729546df
kimsubsub <gustjqv12@naver.com>
2025-08-31 20:47:59 +0900

    refactor(directions): FSD로 분리 (components/constants/hooks/queries/routes)

---
+++ apps/admin/src/features/directions/constants/path.ts
@@ -0,0 +1,9 @@
+// 라우터용
+export const DIRECTIONS_ROUTE = '/directions/' as const
+export const EDIT_DIRECTIONS_ROUTE = '/directions/edit/' as const
+
+// 링크용
+export const PATH_DIRECTIONS = {
+  DIRECTIONS: '/directions',
+  EDIT_DIRECTIONS: '/directions/edit',
+} as const
---
+++ apps/admin/src/features/directions/schemas/index.ts
@@ -0,0 +1,5 @@
+export { $AboutResponse as $DirectionsGetResponse } from '~/apis/community/requests/schemas.gen'
+export {
+  $AboutCreateRequest as $DirectionsCreateRequest,
+  $AboutUpdateRequest as $DirectionsUpdateRequest,
+} from '~/apis/admin/requests/schemas.gen'
