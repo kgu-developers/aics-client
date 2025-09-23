c140e665b85129f7b83872cbd4dba4f725c7067a
yummjin <cho4u4o@gmail.com>
2025-09-23 11:00:46 +0900

    refactor(admin): util 함수 shared 디렉토리로 위치 변경 및 내보내기 추가

--- apps/admin/src/shared/utils/auth.ts
+++ apps/admin/src/shared/utils/auth.ts
@@ -1,8 +1,7 @@
 import { useNavigate } from '@tanstack/react-router'
 
 import { ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY } from '~/constants/api'
-import { removeTokens } from '~/utils/api'
-import { decodeJwt } from '~/utils/jwt'
+import { decodeJwt, removeTokens } from '~/shared/utils'
 
 import type { Tokens } from '~/hooks/use-sign-in'
 
