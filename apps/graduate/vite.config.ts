e35fd994ac6c4d9380dfbcfc780f8fd445208365
cho4u4o <cho4u4o@gmail.com>
2025-10-06 03:15:56 +0900

    chore(graduate): vanilla extract 플러그인 추가

--- apps/graduate/vite.config.ts
+++ apps/graduate/vite.config.ts
@@ -1,14 +1,20 @@
 import { resolve } from 'node:path'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 import viteReact from '@vitejs/plugin-react'
 import { defineConfig } from 'vitest/config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact()],
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+    vanillaExtractPlugin(),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
+      '~': resolve(__dirname, './src'),
     },
   },
   test: {
