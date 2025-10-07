d084211ecbd85a7d16ff411db9d2b9c52d6dd633
kimsubsub <gustjqv12@naver.com>
2025-10-07 23:15:21 +0900

    style(admin): 메인 콘텐츠 배경색·경계선 색상 업데이트

--- apps/graduate/src/routes/admin.tsx
+++ apps/graduate/src/routes/admin.tsx
@@ -1,11 +1,11 @@
-
+import {vars} from '~/vars.css'
 import { Outlet, createFileRoute } from '@tanstack/react-router'
 import { Sidebar } from '~/widgets/sidebar'
 export const Route = createFileRoute('/admin')({
   component: () => (
     <div style={{ display: 'flex' }}>
       <Sidebar />
-      <main style={{ flex: '1 1 auto', overflow: 'auto' }}>
+      <main style={{ flex: '1 1 auto', overflow: 'auto' ,backgroundColor: vars.colors.sub }}>
         <Outlet />
       </main>
     </div>
--- apps/graduate/src/widgets/sidebar/ui/Sidebar.css.ts
+++ apps/graduate/src/widgets/sidebar/ui/Sidebar.css.ts
@@ -5,7 +5,7 @@ export const sidebar = style({
   width: '250px',
   height: '100vh',
   backgroundColor: vars.colors.white,
-  borderRight: `1px solid ${vars.colors.mainLight}`,
+  borderRight: `1px solid ${vars.colors.border}`,
   display: 'flex',
   flexDirection: 'column',
 })
