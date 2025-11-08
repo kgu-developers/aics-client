ffd3e7c1603aaffa84675e4b83493ab1703bab95
seoJing <tjwlsrb1021@naver.com>
2025-11-08 16:04:14 +0900

    refactor(graduate): lint 에러 수정

--- apps/graduate/src/features/all-management/contexts/UserDetailModalContext.tsx
+++ apps/graduate/src/features/all-management/contexts/UserDetailModalContext.tsx
@@ -1,6 +1,6 @@
-import { createContext, useContext, useState, ReactNode } from 'react';
+import { createContext, useContext, useState, type ReactNode } from 'react';
 
-import { Mode } from '../types/allManagement';
+import type { Mode } from '../types/allManagement';
 
 interface UserDetailModalContextType {
   mode: Mode;
