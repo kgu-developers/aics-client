e1de5ddc19c6adb83226e0b9ffb9ecb2af00e698
kimsubsub <gustjqv12@naver.com>
2025-11-25 00:08:09 +0900

    chore: StudentAddModal 기능을 widgets 디렉토리로 이동

--- apps/graduate/src/shared/components/index.ts
+++ apps/graduate/src/shared/components/index.ts
@@ -3,6 +3,5 @@ export * from './Toolbar';
 export * from './Header';
 export * from './Pagination';
 export * from './DataTable';
-export * from './StudentAddModal';
 export * from './Section';
 export { default as ProtectedRoute } from './ProtectedRoute';
