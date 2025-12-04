3855f72edc27a0e19364da6101072d9c714c0604
yummjin <cho4u4o@gmail.com>
2025-12-04 17:55:41 +0900

    feat(graduate): 로그인 성공 시 토큰 저장

--- apps/graduate/src/pages/login/api/submitLogin.ts
+++ apps/graduate/src/pages/login/api/submitLogin.ts
@@ -3,10 +3,11 @@ import { useMutation } from '@tanstack/react-query';
 import { post } from '~/shared/api';
 import { END_POINT } from '~/shared/constants';
 
-import type { LoginFormData } from '../model/login';
+import type { LoginFormData, LoginResponse } from '../model/login';
+import { setAccessToken, setRefreshToken } from '~/shared/utils';
 
 export const submitLogin = async (data: LoginFormData) => {
-  const response = await post({
+  const response = await post<LoginResponse>({
     request: END_POINT.AUTH.LOGIN,
     data,
   });
@@ -16,7 +17,9 @@ export const submitLogin = async (data: LoginFormData) => {
 export const useSubmitLogin = () => {
   return useMutation({
     mutationFn: submitLogin,
-    onSuccess: () => {
+    onSuccess: response => {
+      setAccessToken(response.accessToken);
+      setRefreshToken(response.refreshToken);
       alert(
         '로그인 성공. 토큰을 발급받았습니다. admin 또는 client 페이지로 이동해 작업하세요.',
       );
