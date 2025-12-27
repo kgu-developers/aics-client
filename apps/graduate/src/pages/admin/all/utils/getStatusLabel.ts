2e2ea7f50ccb3d4bdd7674c0b51cffb7012b70b5
seoJing <tjwlsrb1021@naver.com>
2025-12-28 00:39:08 +0900

    refactor(graduation): 중첩 if문 switch문으로 변경

--- apps/graduate/src/pages/admin/all/utils/getStatusLabel.ts
+++ apps/graduate/src/pages/admin/all/utils/getStatusLabel.ts
@@ -12,33 +12,34 @@ import {
 import type { StudentStatus } from '../types/allManagement';
 
 export function getStatusLabel(status: StudentStatus | undefined): string {
-  if (!status) {
-    return STATUS_UNKNOWN;
-  }
+  switch (status?.type) {
+    case 'CERTIFICATE':
+      if (!status.certificate.submitted) {
+        return STATUS_CERTIFICATE_NOT_SUBMITTED;
+      }
+      if (!status.certificate.approval) {
+        return STATUS_CERTIFICATE_SUBMITTED;
+      }
+      return STATUS_CERTIFICATE_APPROVED;
 
-  if (status.type === 'CERTIFICATE') {
-    if (!status.certificate.submitted) {
-      return STATUS_CERTIFICATE_NOT_SUBMITTED;
-    }
-    if (!status.certificate.approval) {
-      return STATUS_CERTIFICATE_SUBMITTED;
-    }
-    return STATUS_CERTIFICATE_APPROVED;
-  }
+    case 'THESIS':
+      if (!status.midThesis.submitted) {
+        return STATUS_MID_REPORT_NOT_SUBMITTED;
+      }
+      if (!status.midThesis.approval) {
+        return STATUS_MID_REPORT_SUBMITTED;
+      }
 
-  if (!status.midThesis.submitted) {
-    return STATUS_MID_REPORT_NOT_SUBMITTED;
-  }
-  if (!status.midThesis.approval) {
-    return STATUS_MID_REPORT_SUBMITTED;
-  }
+      if (!status.finalThesis.submitted) {
+        return STATUS_FINAL_REPORT_NOT_SUBMITTED;
+      }
+      if (!status.finalThesis.approval) {
+        return STATUS_FINAL_REPORT_SUBMITTED;
+      }
 
-  if (!status.finalThesis.submitted) {
-    return STATUS_FINAL_REPORT_NOT_SUBMITTED;
-  }
-  if (!status.finalThesis.approval) {
-    return STATUS_FINAL_REPORT_SUBMITTED;
-  }
+      return STATUS_FINAL_REPORT_APPROVED;
 
-  return STATUS_FINAL_REPORT_APPROVED;
+    default:
+      return STATUS_UNKNOWN;
+  }
 }
