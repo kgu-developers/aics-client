a1b2fd5d30d7ca216815c275ea5c0612a47a2ea4
cho4u4o <cho4u4o@gmail.com>
2025-12-28 03:46:29 +0900

    feat(graduate): 로그아웃 시 새로고침 적용

--- apps/graduate/src/shared/utils/logout.ts
+++ apps/graduate/src/shared/utils/logout.ts
@@ -1,12 +1,6 @@
-import { ROUTE } from '../constants';
-import { useAuthStore } from '../stores';
-import router from './router';
 import { clearTokens } from './token';
 
 export default function logout(): void {
   clearTokens();
-  const { setIsAuthenticated, setIsAdmin } = useAuthStore.getState();
-  setIsAuthenticated(false);
-  setIsAdmin(false);
-  router.navigate({ to: ROUTE.LOGIN });
+  window.location.reload();
 }
