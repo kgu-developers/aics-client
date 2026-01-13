f92dd039e0a3e3b053cfec794e702a7c290b9232
kimsubsub <gustjqv12@naver.com>
2026-01-13 21:05:35 +0900

    refactor(graduate): 승인 처리 공통 훅 분리

--- apps/graduate/src/shared/hooks/student/index.ts
+++ apps/graduate/src/shared/hooks/student/index.ts
@@ -1,3 +1,4 @@
 export { useFetchGraduationUsers } from './useFetchGraduationUsers';
+export { useApproveGraduationUsers } from './useApproveGraduationUsers';
 export { useRemoveGraduationUsers } from './useRemoveGraduationUsers';
 export { useUpdateGraduationUsersBatchApprove } from './useUpdateGraduationUsersBatchApprove';
