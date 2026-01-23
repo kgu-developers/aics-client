e7c414d40b7269a4946709388c7aeaa64694a09a
seoJing <tjwlsrb1021@naver.com>
2026-01-23 15:27:16 +0900

    refactor(graduate): admin 사이드 초기 페이지 설정

--- apps/graduate/src/routes/_afterLogin/index.tsx
+++ apps/graduate/src/routes/_afterLogin/index.tsx
@@ -1,16 +1,13 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 import { lazy } from 'react';
 
 const HomePage = lazy(() => import('~/pages/client/home'));
 
 export const Route = createFileRoute('/_afterLogin/')({
-  component: App,
+  beforeLoad: ({ context }) => {
+    if (context.auth.isAdmin) {
+      throw redirect({ to: '/all' });
+    }
+  },
+  component: HomePage,
 });
-
-function App() {
-  const { auth } = Route.useRouteContext();
-  if (!auth.isAdmin) {
-    return <HomePage />;
-  }
-  return <>작업을 선택하세요</>;
-}
